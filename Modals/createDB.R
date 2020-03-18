e6960c522bc16804a44adcb8ef7726b25fb1b50d
Anantadinath <vickey.coool@gmail.com>
2020-03-18 11:41:13 +0530

    changes

--- Modals/createDB.R
+++ Modals/createDB.R
@@ -21,3 +21,5 @@ MainDB %>%
     write,
     overwrite = TRUE
   )
+
+
--- Modules/cars_table_module.R
+++ Modules/cars_table_module.R
@@ -1,25 +1,25 @@
 defects_ui <- function(id) {
 
-# namespace ---------------------------------------------------------------
-
+  # namespace ---------------------------------------------------------------
+  
   ns <- NS(id)
   
+  # ui elements -------------------------------------------------------------
+  
   tagList(
-    fluidRow(
-      column(
-        width = 2,
-        actionButton(
-          ns("add_car"),
-          "Add",
-          class = "btn-success",
-          style = "color: #fff;",
-          icon = icon('plus'),
-          width = '100%'
-        ),
-        tags$br(),
-        tags$br()
-      )
-    ),
+    fluidRow(column(
+      width = 2,
+      actionButton(
+        ns("add_car"),
+        "Add",
+        class = "btn-success",
+        style = "color: #fff;",
+        icon = icon('plus'),
+        width = '100%'
+      ),
+      tags$br(),
+      tags$br()
+    )),
     fluidRow(
       column(
         width = 12,
@@ -35,11 +35,13 @@ defects_ui <- function(id) {
   )
 }
 
+# server ------------------------------------------------------------------
+
 defects_server <- function(input, output, session) {
+
+  # main Data ---------------------------------------------------------------
   
-  # Read in "mtcars" table from the database
   cars <- reactive({
-    
     session$userData$db_trigger()
     
     session$userData$conn %>%
@@ -57,6 +59,8 @@ defects_server <- function(input, output, session) {
   
   car_table_prep <- reactiveVal(NULL)
   
+  # changes in the Data -----------------------------------------------------
+  
   observeEvent(cars(), {
     out <- cars()
     
@@ -65,21 +69,23 @@ defects_server <- function(input, output, session) {
     actions <- purrr::map_chr(ids, function(id_) {
       paste0(
         '<div class="btn-group" style="width: 75px;" role="group" aria-label="Basic example">
-          <button class="btn btn-primary btn-sm edit_btn" data-toggle="tooltip" data-placement="top" title="Edit" id = ', id_, ' style="margin: 0"><i class="fa fa-pencil-square-o"></i></button>
-          <button class="btn btn-danger btn-sm delete_btn" data-toggle="tooltip" data-placement="top" title="Delete" id = ', id_, ' style="margin: 0"><i class="fa fa-trash-o"></i></button>
+          <button class="btn btn-primary btn-sm edit_btn" data-toggle="tooltip" data-placement="top" title="Edit" id = ',
+        id_,
+        ' style="margin: 0"><i class="fa fa-pencil-square-o"></i></button>
+          <button class="btn btn-danger btn-sm delete_btn" data-toggle="tooltip" data-placement="top" title="Delete" id = ',
+        id_,
+        ' style="margin: 0"><i class="fa fa-trash-o"></i></button>
         </div>'
       )
     })
     
     # Remove the `uid` column. We don't want to show this column to the user
     out <- out %>%
-      select(-uid, -is_deleted)
+      select(-uid,-is_deleted)
     
     # Set the Action Buttons row to the first column of the `mtcars` table
-    out <- cbind(
-      tibble(" " = actions),
-      out
-    )
+    out <- cbind(tibble(" " = actions),
+                 out)
     
     if (is.null(car_table_prep())) {
       # loading data into the table for the first time, so we render the entire table
@@ -87,13 +93,18 @@ defects_server <- function(input, output, session) {
       car_table_prep(out)
       
     } else {
-      
       # table has already rendered, so use DT proxy to update the data in the
       # table without rerendering the entire table
-      replaceData(car_table_proxy, out, resetPaging = FALSE, rownames = FALSE)
+      replaceData(car_table_proxy,
+                  out,
+                  resetPaging = FALSE,
+                  rownames = FALSE)
       
     }
   })
+  
+  
+  # render Table ------------------------------------------------------------
   
   output$car_table <- renderDT({
     req(car_table_prep())
@@ -102,10 +113,24 @@ defects_server <- function(input, output, session) {
     datatable(
       out,
       rownames = FALSE,
-      colnames = c('Model', 'Miles/Gallon', 'Cylinders', 'Displacement (cu.in.)',
-                   'Horsepower', 'Rear Axle Ratio', 'Weight (lbs)', '1/4 Mile Time',
-                   'Engine', 'Transmission', 'Forward Gears', 'Carburetors', 'Created At',
-                   'Created By', 'Modified At', 'Modified By'),
+      colnames = c(
+        'Model',
+        'Miles/Gallon',
+        'Cylinders',
+        'Displacement (cu.in.)',
+        'Horsepower',
+        'Rear Axle Ratio',
+        'Weight (lbs)',
+        '1/4 Mile Time',
+        'Engine',
+        'Transmission',
+        'Forward Gears',
+        'Carburetors',
+        'Created At',
+        'Created By',
+        'Modified At',
+        'Modified By'
+      ),
       selection = "none",
       class = "compact stripe row-border nowrap",
       # Escape the HTML in all except 1st column (which has the buttons)
@@ -119,49 +144,54 @@ defects_server <- function(input, output, session) {
             extend = "excel",
             text = "Download",
             title = paste0("mtcars-", Sys.Date()),
-            exportOptions = list(
-              columns = 1:(length(out) - 1)
-            )
+            exportOptions = list(columns = 1:(length(out) - 1))
           )
         ),
-        columnDefs = list(
-          list(targets = 0, orderable = FALSE)
-        )
+        columnDefs = list(list(
+          targets = 0, orderable = FALSE
+        ))
       )
     ) %>%
-      formatDate(
-        columns = c("created_at", "modified_at"),
-        method = 'toLocaleString'
-      )
+      formatDate(columns = c("created_at", "modified_at"),
+                 method = 'toLocaleString')
     
   })
   
   car_table_proxy <- DT::dataTableProxy('car_table')
   
+  # edit data ---------------------------------------------------------------
+  
   callModule(
     car_edit_module,
     "add_car",
     modal_title = "Add Car",
-    car_to_edit = function() NULL,
-    modal_trigger = reactive({input$add_car})
+    car_to_edit = function()
+      NULL,
+    modal_trigger = reactive({
+      input$add_car
+    })
   )
   
   car_to_edit <- eventReactive(input$car_id_to_edit, {
-    
     cars() %>%
       filter(uid == input$car_id_to_edit)
   })
+  
+  
   
   callModule(
     car_edit_module,
     "edit_car",
     modal_title = "Edit Car",
     car_to_edit = car_to_edit,
-    modal_trigger = reactive({input$car_id_to_edit})
+    modal_trigger = reactive({
+      input$car_id_to_edit
+    })
   )
   
+  # delete data -------------------------------------------------------------
+  
   car_to_delete <- eventReactive(input$car_id_to_delete, {
-    
     out <- cars() %>%
       filter(uid == input$car_id_to_delete) %>%
       pull(model)
@@ -174,7 +204,9 @@ defects_server <- function(input, output, session) {
     "delete_car",
     modal_title = "Delete Car",
     car_to_delete = car_to_delete,
-    modal_trigger = reactive({input$car_id_to_delete})
+    modal_trigger = reactive({
+      input$car_id_to_delete
+    })
   )
-
+  
 }
