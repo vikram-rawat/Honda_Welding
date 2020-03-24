9a29a2026c3a4c95e9e128af7a4bc3b214ea91c3
Anantadinath <vickey.coool@gmail.com>
2020-03-24 13:45:08 +0530

    updated button clicks

--- www/js/DailyFeed.js
+++ www/js/DailyFeed.js
@@ -18,8 +18,20 @@ var dailyFeed = new Vue({
     show: {},
     inputValue: {},
     apiData: {
-      Zones: ["Zone 1", "Zone 2", "Zone 3", "Zone 4"],
-      Cars: ["Toyota", "Suzuki", "Honda", "SomeModal"],
+      Zones: [
+        { name: "Zone 1", classes: "disabled" },
+        { name: "Zone 2", classes: "disabled" },
+        { name: "Zone 3", classes: "disabled" },
+        { name: "Zone 4", classes: "disabled" }
+      ],
+      Cars: [
+        { name: "Maruti", classes: "disabled" },
+        { name: "Maruti2", classes: "disabled" },
+        { name: "Maruti3", classes: "disabled" },
+        { name: "Maruti4", classes: "disabled" },
+        { name: "Maruti5", classes: "disabled" },
+        { name: "Maruti6", classes: "disabled" }
+      ],
       Defects: ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"]
     }
   },
@@ -28,7 +40,7 @@ var dailyFeed = new Vue({
       this.disable.morningShift = "active";
       this.disable.noonShift = "disabled";
       this.disable.nightShift = "disabled";
-      console.log("button Clicked Morning");
+
       Shiny.setInputValue("daily_data-Shifts", "Morning", {
         priority: "event"
       });
@@ -37,7 +49,6 @@ var dailyFeed = new Vue({
       this.disable.morningShift = "disabled";
       this.disable.noonShift = "active";
       this.disable.nightShift = "disabled";
-      console.log("button Clicked Noon");
 
       Shiny.setInputValue("daily_data-Shifts", "Noon", { priority: "event" });
     },
@@ -45,7 +56,6 @@ var dailyFeed = new Vue({
       this.disable.morningShift = "disabled";
       this.disable.noonShift = "disabled";
       this.disable.nightShift = "active";
-      console.log("button Clicked Night");
 
       Shiny.setInputValue("daily_data-Shifts", "Night", { priority: "event" });
     },
@@ -54,6 +64,50 @@ var dailyFeed = new Vue({
       this.disable.noonShift = "active";
       this.disable.nightShift = "active";
       Shiny.setInputValue("daily_data-Shifts", "", { priority: "event" });
+    },
+    zoneClick: function(zone) {
+      for (x in this.apiData.Zones) {
+        value = this.apiData.Zones[x];
+        if (value.name != zone.name) {
+          value.classes = "disabled";
+        } else {
+          value.classes = "active";
+        }
+      }
+      Shiny.setInputValue("daily_data-Zones", value.name, {
+        priority: "event"
+      });
+    },
+    zoneClearAll: function() {
+      for (x in this.apiData.Zones) {
+        value = this.apiData.Zones[x];
+        value.classes = "disabled";
+      }
+      Shiny.setInputValue("daily_data-Zones", "", {
+        priority: "event"
+      });
+    },
+    carClearAll: function() {
+      for (x in this.apiData.Cars) {
+        value = this.apiData.Cars[x];
+        value.classes = "disabled";
+      }
+      Shiny.setInputValue("daily_data-Zones", "", {
+        priority: "event"
+      });
+    },
+    carClick: function(car) {
+      for (x in this.apiData.Cars) {
+        value = this.apiData.Cars[x];
+        if (value.name != car.name) {
+          value.classes = "disabled";
+        } else {
+          value.classes = "active";
+        }
+      }
+      Shiny.setInputValue("daily_data-Zones", value.name, {
+        priority: "event"
+      });
     }
   },
   mounted: function() {},
